94cebd168cf6c370a407968566dd58812d0a9899
zainab-zahid-khokhar <zainabzahid9760@gmail.com>
2025-04-17 23:12:10 +0500

    min-max heap functions

--- min-max-heap.cpp
+++ min-max-heap.cpp
@@ -1,19 +1,119 @@
 #include "min-max-heap.hpp"
 
+
+
 template <class T>
-int Heap<T>::parent(int i)
+bool MinMaxHeap<T>::isMinLevel(int i){
+    return log2(zindex + 1) % 2 == 0;
+}
+
+template <class T>
+int MinMaxHeap<T>::grandparent(int i){
+   return parent(parent(i));
+}
+
+template <class T>
+int MinMaxHeap<T>::parent(int i)
 {
     return (i - 1) / 2;
 }
 
 template <class T>
-int Heap<T>::left_child(int i)
+int MinMaxHeap<T>::left_child(int i)
 {
     return 2 * i + 1;
 }
 
 template <class T>
-int Heap<T>::right_child(int i)
+int MinMaxHeap<T>::right_child(int i)
 {
     return 2 * i + 2;
 }
+
+template <class T>
+void MinMaxHeap<T>::bubble_up(int i){
+
+    if(i==0)
+    return; //cant bubble upe from here 
+
+    int parent = parent(i); //get parent
+
+    if (isMinLevel(i)){ //for min level
+        if(arr[i]>arr[parent]) {// if value is greater than parent shift it to a max level
+            T temp = arr[i]; //swap
+            arr[i] = arr[parent];
+            arr[parent] = temp;
+            bubble_up_max(i); //it is now in max level 
+        }else{
+            bubble_up_min(i);
+        }
+    }else{
+        if(arr[i]<arr[parent]) {// if value is lesser than parent shift it to a min level
+            T temp = arr[i]; //swap
+            arr[i] = arr[parent];
+            arr[parent] = temp;
+            bubble_up_min(i); //it is now in min level 
+        }else{
+            bubble_up_max(i);
+        }
+    }
+
+
+}
+
+template <class T>
+void MinMaxHeap<T>::bubble_up_min(int i){
+    int grandparent = grandparent(i);
+    while(i >2 && ( arr[i] < arr[grandparent]) ){ //check for i >2 to make sure i has a grandparent (is a deeper level node)
+        T temp = arr[i]; //swap
+        arr[i] = arr[grandparent];
+        arr[grandparent] = temp;
+        i = grandparent; 
+        grandparent = grandparent(i);
+    }
+
+}
+
+template <class T>
+void MinMaxHeap<T>::bubble_up_max(int i){
+    int grandparent = grandparent(i);
+    while(i >2 && ( arr[i] > arr[grandparent]) ){
+        T temp = arr[i]; //swap
+        arr[i] = arr[grandparent];
+        arr[grandparent] = temp;
+        i = grandparent; 
+        grandparent = grandparent(i);
+    }
+
+
+}
+
+template <class T>
+void MinMaxHeap<T>::insert(T x){
+
+    //resize?
+    arr[size] = x;
+    size++;
+    bubble_up(size-1);
+
+} 
+
+
+template <class T>
+void MinMaxHeap<T>::trickle_down(int i){
+
+    
+}
+
+
+
+template <class T>
+bool MinMaxHeap<T>::replace(int i, T x){
+
+}
+
+template <class T>
+bool MinMaxHeap<T>::delete_value(int i){
+
+}
+
--- min-max-heap.hpp
+++ min-max-heap.hpp
@@ -1,27 +1,32 @@
 #pragma once
+#include <vector>
 
 // both max and min
 template <class T>
-class Heap
+class MinMaxHeap
 {
 
 private:
-    array<T> arr;
+    std::vector<T> arr;
     int size;
 
 public:
+    MinMaxHeap(){size = 0;};
+    bool isMinLevel(int i);
+    int grandparent(int i);
     int parent(int i);
     int left_child(int i);
     int right_child(int i);
     T root() { return arr[0]; }
-};
+    
+    void bubble_up(int i);
+    void bubble_up_min(int i);
+    void bubble_up_max(int i);
+    void insert(T x);
 
-template <class T>
-class MinHeap : public Heap
-{
-};
+    void trickle_down(int i);
+    void insert(T x);
+    bool replace(int i, T x);
+    bool delete_value(int i);
 
-template <class T>
-class MaxHeap : public Heap
-{
 };
